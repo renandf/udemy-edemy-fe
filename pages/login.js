cf21130ec9205ffbc7a2155adfe85705f4863996
Renan Castro <renandf@gmail.com>
2022-01-21 07:47:19 +1100

    feat: Create Login, Register, and TopNav base structure

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="jumbotron">Login</h1>
+      <p>Login page</p>
+    </>
+  )
+}
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+  return (
+    <>
+      <h1 className="jumbotron">Register</h1>
+      <p>Register page</p>
+    </>
+  )
+}
+
+export default Register;
