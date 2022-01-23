2d522d9fe7f8ea95ea082aa45a5a00039c78f777
Renan Castro <renandf@gmail.com>
2022-01-23 18:34:49 +1100

    feat: Update TopNav to reflect the active page every time

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Menu } from 'antd';
 import Link from 'next/link';
 import { AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/icons';
@@ -5,21 +6,39 @@ import { AppstoreOutlined, LoginOutlined, UserAddOutlined } from '@ant-design/ic
 const { Item } = Menu;
 
 const TopNav = () => {
+  const [current, setCurrent] = useState("");
+
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname)
+  }, [process.browser && window.location.pathname])
+
   return (
-    <Menu mode="horizontal">
-      <Item icon={<AppstoreOutlined />}>
+    <Menu mode="horizontal" selectedKeys={[current]}>
+      <Item
+        key="/"
+        icon={<AppstoreOutlined />}
+        onClick={(e) => setCurrent(e.key)}
+      >
         <Link href="/">
           <a>App</a>
         </Link>
       </Item>
 
-      <Item icon={<LoginOutlined />}>
+      <Item
+        key="/login"
+        icon={<LoginOutlined />}
+        onClick={(e) => setCurrent(e.key)}
+      >
         <Link href="/login">
           <a>Login</a>
         </Link>
       </Item>
 
-      <Item icon={<UserAddOutlined />}>
+      <Item
+        key="/register"
+        icon={<UserAddOutlined />}
+        onClick={(e) => setCurrent(e.key)}
+      >
         <Link href="/register">
           <a>Register</a>
         </Link>
